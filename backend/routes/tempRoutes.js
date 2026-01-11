0e74aed909ea0e2f075835d3acdc587033e10358
DeshanChinthaka <165351347+DeshanChinthaka@users.noreply.github.com>
2026-01-11 23:57:32 +0530

    Create tempRoutes.js

---
+++ backend/routes/tempRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { convertTemp } = require('../controllers/tempController');
+
+const router = express.Router();
+
+router.post('/convert', convertTemp);
+
+module.exports = router;
